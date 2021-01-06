bf2f18ebb8480392453580eeb2ce557734c41da1
Shamnas <shamnastv@gmail.com>
2021-01-06 12:13:53 +0530

    update

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--epochs', type=int, default=400,
                         help='number of epochs to train (default: 350)')
-    parser.add_argument('--lr', type=float, default=0.0001,
+    parser.add_argument('--lr', type=float, default=0.0005,
                         help='learning rate (default: 0.01)')
     parser.add_argument('--seed', type=int, default=0,
                         help='random seed for splitting the dataset into 10 (default: 0)')
