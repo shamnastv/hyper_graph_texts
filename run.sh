a59f439b97c92fa909ca6e1cbf7e06522619a79c
Shamnas <shamnastv@gmail.com>
2021-01-10 09:08:00 +0530

    update

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
+python3 main.py --lr 1e-2 --hidden_dim 100
+python3 main.py --lr 1e-3 --hidden_dim 100
 python3 main.py --lr 1e-4 --hidden_dim 100
-python3 main.py --lr 5e-5 --hidden_dim 100
 python3 main.py --lr 1e-5 --hidden_dim 100
