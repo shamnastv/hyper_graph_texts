54193d4c55ab4e1b16befe1d97754c87a3cc6c4d
Shamnas <shamnastv@gmail.com>
2021-04-04 14:07:41 +0530

    update

--- layer.py
+++ layer.py
@@ -72,8 +72,8 @@ class HGNNLayer(nn.Module):
         h = self.mlp(h)
         h_n = self.message_passing_3_1(incident_mat_full, h, degree_v_full)
         h_n = self.activation(h_n)
-        h_n = self.dropout(h_n)
-        h_n = self.batch_norms(h_n)
+        # h_n = self.dropout(h_n)
+        # h_n = self.batch_norms(h_n)
 
         h_n = self.mlp2(h_n)
         h_n = self.message_passing_3_2(incident_mat_full, h_n, degree_e_full)
