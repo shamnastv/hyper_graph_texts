8e51333761aedd0e4b3e4b722e6736b2ab407179
Shamnas <shamnastv@gmail.com>
2021-04-22 19:59:32 +0530

    update

--- 2.sh
+++ 2.sh
@@ -6,9 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
+python3 main.py --lr 1e-3 --hidden_dim 50 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --early_stop 20
+python3 main.py --lr 1e-3 --hidden_dim 100 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --early_stop 20
 python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --early_stop 20
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 2 --dataset R8 --early_stop 20
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 4 --dropout .3 --num_mlp_layers 1 --dataset R8 --early_stop 20
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 4 --dropout .3 --num_mlp_layers 2 --dataset R8 --early_stop 20
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 5 --dropout .3 --num_mlp_layers 1 --dataset R8 --early_stop 20
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 5 --dropout .3 --num_mlp_layers 2 --dataset R8 --early_stop 20
+python3 main.py --lr 1e-3 --hidden_dim 300 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --early_stop 20
