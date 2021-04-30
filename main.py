5986e225bd5e744d3129c8886abf82c09889f2d6
Shamnas <shamnastv@gmail.com>
2021-04-30 10:47:49 +0530

    update

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def main():
         = get_data(dataset=args.dataset, lda=args.lda, seed=args.seed)
 
     num_classes = len(labels_dic)
-    num_clusters = num_classes
+    num_clusters = (num_classes + 1) // 2
     # train_size, dev_size, test_size = len(train_data), len(dev_data), len(test_data)
     data_full = train_data + dev_data + test_data
 
