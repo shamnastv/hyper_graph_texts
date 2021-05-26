d559e75fe9369caf22b4817a5997b5a4c7c4e784
Shamnas <shamnastv@gmail.com>
2021-05-26 13:38:24 +0530

    update

--- r8.sh
+++ r8.sh
@@ -6,8 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --num_mlp_layers 2 --dataset R8 --seed -1
-python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 4 --num_mlp_layers 1 --dataset R8 --seed -1
+python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --num_mlp_layers 1 --dataset R8 --seed -1 --lda
 
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --lda
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --random_vec
