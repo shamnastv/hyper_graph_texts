d05fd4d6b1c39c67d9316b6315d63c66113f7463
Shamnas <shamnastv@gmail.com>
2021-06-09 10:03:52 +0530

    update

--- ohsumed.sh
+++ ohsumed.sh
@@ -10,4 +10,4 @@
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset ohsumed --lda
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset ohsumed --random_vec
 
-python3 main.py --hidden_dim 200 --num_layers 3 --num_mlp_layers 1 --dataset ohsumed --seed -1
+python3 main.py --num_layers 3 --num_mlp_layers 1 --dataset ohsumed --seed -1
