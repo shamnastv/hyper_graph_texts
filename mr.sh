0a10ecad415cb4c9a26560228b584715dae3f40e
Shamnas <shamnastv@gmail.com>
2021-06-13 22:17:13 +0530

    update

--- mr.sh
+++ mr.sh
@@ -6,14 +6,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-4  --weight_decay 1e-4
-python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-3  --weight_decay 1e-4
+python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-4  --weight_decay 1e-5
+python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 1e-3  --weight_decay 1e-7
+python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 1e-3  --weight_decay 1e-6
 
-python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-4  --weight_decay 1e-7
-python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-3  --weight_decay 1e-7
-
-python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-4  --weight_decay 1e-12
-python3 main.py --num_layers 3 --dataset mr --seed -1 --num_clusters 1 --early_stop 10 --lr 5e-3  --weight_decay 1e-12
 
 #python3 main.py --lr 5e-3 --num_layers 3 --num_mlp_layers 1 --dataset mr --seed -1 --num_clusters 1
 #python3 main.py --lr 5e-4 --num_layers 3 --num_mlp_layers 1 --dataset mr --seed -1 --num_clusters 1
--- r8.sh
+++ r8.sh
@@ -6,14 +6,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-4 --lr 0.0005
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-4 --lr 0.005
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-6 --lr 0.001
 
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-7 --lr 0.0005
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-7 --lr 0.005
-
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-12 --lr 0.0005
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-12 --lr 0.005
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-6 --lr 0.001
 
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --lda
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R8 --random_vec
