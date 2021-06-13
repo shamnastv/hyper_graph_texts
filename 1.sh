3e58248bf668978d9b75faaf8407307e76e189ea
Shamnas <shamnastv@gmail.com>
2021-06-14 01:35:35 +0530

    update

--- 1.sh
+++ 1.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-#SBATCH --job-name=20NG # Job name
-#SBATCH --ntasks=8 # Run on a single CPU
+#SBATCH --job-name=Ablation # Job name
+#SBATCH --ntasks=4 # Run on a single CPU
 #SBATCH --time=47:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-#python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset 20ng
-#python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset 20ng --lda
-#python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset 20ng --random_vec
-
-python3 main.py --num_layers 3 --num_mlp_layers 1 --dataset 20ng --seed -1
-python3 main.py --num_layers 3 --num_mlp_layers 1 --dataset 20ng --seed -1 --lr 5e-3
+python3 main.py --num_layers 3 --dataset ohsumed --seed -1 --num_clusters 0 --num_exp 4
+python3 main.py --num_layers 3 --dataset ohsumed --seed -1 --num_clusters 1 --num_exp 4
+python3 main.py --num_layers 3 --dataset ohsumed --seed -1 --num_clusters 3 --num_exp 4 --tsne
