d0db0422602fa9c80acb0aa431ef130bff9285a4
Shamnas <shamnastv@gmail.com>
2021-06-15 00:24:05 +0530

    update

--- 2.sh
+++ 2.sh
@@ -5,6 +5,7 @@
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --early_stop 10 --weight_decay 1e-7 --lr 1e-2 --num_exp 2 --dropout .5
 
-python3 main.py --num_layers 3 --dataset 20ng --seed -1 --num_clusters 1 --num_exp 4
+#python3 main.py --num_layers 3 --dataset 20ng --seed -1 --num_clusters 1 --num_exp 4
 
