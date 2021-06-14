0a688e7c920ef0433fd4e1cb0a448bd4a3940b00
Shamnas <shamnastv@gmail.com>
2021-06-14 18:06:30 +0530

    update

--- r52.sh
+++ r52.sh
@@ -6,6 +6,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --num_layers 3 --num_mlp_layers 1 --dataset R52 --seed -1
+python3 main.py --num_layers 3 --dataset R52 --seed -1  --early_stop 10 --weight_decay 1e-7 --lr 5e-4
+python3 main.py --num_layers 3 --dataset R52 --seed -1  --early_stop 10 --weight_decay 1e-7 --lr 1e-3
+python3 main.py --num_layers 3 --dataset R52 --seed -1  --early_stop 10 --weight_decay 1e-7 --lr 5e-3
+
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R52 --lda
 #python3 main.py --lr 1e-3 --hidden_dim 200 --num_layers 3 --dropout .3 --num_mlp_layers 1 --dataset R52 --random_vec
