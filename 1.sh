f350f40d4af73756a2554a7f249b7bf1a432733f
Shamnas <shamnastv@gmail.com>
2021-06-15 18:03:31 +0530

    update

--- 1.sh
+++ 1.sh
@@ -6,5 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 2 --num_exp 2 --early_stop 15 --val_prop .975
-python3 main.py --num_layers 3 --dataset ohsumed --seed -1 --num_clusters 3 --num_exp 2 --early_stop 15 --val_prop .975
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 4 --num_exp 2 --early_stop 10
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 8 --num_exp 2 --early_stop 10
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 16 --num_exp 2 --early_stop 10
+python3 main.py --num_layers 3 --dataset R8 --seed -1 --num_clusters 32 --num_exp 2 --early_stop 10
