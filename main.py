5b28d31d60b2d5a6b7c6cd345c34b2594dec6fca
Shamnas <shamnastv@gmail.com>
2021-07-03 00:13:51 +0530

    update

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ def main():
             # if epoch > 60:
             #     num_clusters = num_classes
 
-            if epoch < 15:
+            if epoch < 10:
                 scheduler.step()
             #     # scheduler2.step()
                 print('Epoch-{0} lr: {1}'.format(epoch, optimizer.param_groups[0]['lr']))
